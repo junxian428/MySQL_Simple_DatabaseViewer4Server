e5f47fce69f5092c74d1c6692c8a57636b774c0b
junxian428 <wengyin777@gmail.com>
2023-09-01 14:04:39 +0800

    update

--- src/main/java/com/example/springprocessmanagementelasticbeanstalkdeploy/FileUploadController/RestDatabase.java
+++ src/main/java/com/example/springprocessmanagementelasticbeanstalkdeploy/FileUploadController/RestDatabase.java
@@ -1,5 +1,7 @@
 package com.example.springprocessmanagementelasticbeanstalkdeploy.FileUploadController;
 
+import static org.mockito.Answers.values;
+
 import java.util.Map;
 
 import javax.sql.DataSource;
@@ -10,6 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import io.micrometer.common.util.StringUtils;
 
 @RestController
 @CrossOrigin("")
@@ -29,23 +35,50 @@ public class RestDatabase {
     
         @PostMapping("/edit-row")
     @ResponseBody
-    public String editRow(@RequestParam String databaseName, @RequestParam String tableName, @RequestParam String rowId, @RequestParam String[] values) {
+    public String editRow(@RequestParam String databaseName, @RequestParam String tableName, @RequestParam String rowId, @RequestParam String values,@RequestParam String keys ) {
         // Create the SET clause for the UPDATE statement
-        System.out.println("API is called");
+        //System.out.println("API is called");
 
         //System.out.println(values);
-        for (int i = 0; i < values.length; i++) {
-            System.out.println("Element at index " + i + ": " + values[i]);
-        }
+        //for (int i = 0; i < values.length; i++) {
+        //    System.out.println("Element at index " + i + ": " + values[i]);
+        //}
         //System.out.println(setClause);
         //setClause.delete(setClause.length() - 2, setClause.length()); // Remove trailing comma and space
     
         // Create the WHERE clause using rowId (assuming it's a primary key)
-        String whereClause = "id = " + rowId; // Replace "id" with your actual primary key column name
+        //String whereClause = "id = " + rowId; // Replace "id" with your actual primary key column name
     
         // Construct the complete SQL update statement
-        String updateSql = "UPDATE " + databaseName + "." + tableName + " SET " + values + " WHERE " + whereClause;
-    
+        //String updateSql = "UPDATE " + databaseName + "." + tableName + " SET " + values + " WHERE " + whereClause;
+        try{
+            System.out.println(values.toString());
+            System.out.println(keys.toString());
+            if (!StringUtils.isEmpty(values)) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String[] valuesArray = objectMapper.readValue(values, String[].class);
+            ObjectMapper objectMapper2 = new ObjectMapper();
+            String[] keysArray = objectMapper2.readValue(keys, String[].class);
+            // Now you have the array of values
+            //System.out.println("Array Length: " + valuesArray.length);
+         
+            for (int i = 0; i < valuesArray.length; i++) {
+                String key = keysArray[i];
+                String value = valuesArray[i];
+                System.out.println("Key: " + key + ", Value: " + value);
+            }
+            // Perform the database update using values, databaseName, tableName, and rowId
+            
+            return "Success"; // Return a success response
+        } else {
+            return "Error: Empty jsonUpdate";
+        }
+        }catch(Exception e){
+            System.out.println(e);
+            return "Error";
+        }
+     
+        /*  
         try {
             jdbcTemplate.update(updateSql);
             return "Success";
@@ -54,6 +87,7 @@ public class RestDatabase {
             System.out.println("Error " + e);
             return "Error";
         }
+        */
     }
     
 
